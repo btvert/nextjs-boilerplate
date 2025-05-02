0cc697f43eb59cfecd7aa8ea2dbe10d16dc35eb4
btvert <0x65d@proton.me>
2025-05-02 12:41:50 -0400

    Update UserGrid.tsx

--- components/UserGrid.tsx
+++ components/UserGrid.tsx
@@ -6,23 +6,30 @@ export default function UserGrid() {
   const tiles = generateGrid();
 
   return (
-    <div
-      className="fixed top-0 left-0 w-screen h-screen grid"
-      style={{
-        gridTemplateColumns: "repeat(48, 1fr)",
-        gridTemplateRows: "repeat(27, 1fr)",
-      }}
-    >
-      {tiles.map((tile) => (
-        <div
-          key={tile}
-          style={{
-            backgroundColor: "#1a1a1a",
-            width: "100%",
-            height: "100%",
-          }}
-        />
-      ))}
+    <div className="w-screen h-screen overflow-hidden bg-black flex items-center justify-center">
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(48, 1fr)",
+          gridTemplateRows: "repeat(27, 1fr)",
+          width: "1920px", // 48 * 40
+          height: "1080px", // 27 * 40
+          transform: "scale(calc(100vw / 1920))",
+          transformOrigin: "top left",
+        }}
+      >
+        {tiles.map((tile) => (
+          <div
+            key={tile}
+            style={{
+              backgroundColor: "#1a1a1a",
+              border: "0px solid transparent",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
