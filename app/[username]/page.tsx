23e70fd75a7f7000ed6113493736a6f81ed175d8
btvert <0x65d@proton.me>
2025-05-02 10:41:52 -0400

    Create page.tsx

---
+++ app/[username]/page.tsx
@@ -0,0 +1,20 @@
+import UserGrid from "@/components/UserGrid";
+
+type Props = {
+  params: {
+    username: string;
+  };
+};
+
+export default function UserPage({ params }: Props) {
+  const { username } = params;
+
+  return (
+    <div className="h-screen grid place-items-center text-center px-6">
+      <main className="flex flex-col gap-6 items-center">
+        <h1 className="text-2xl font-bold">{username}'s Board</h1>
+        <UserGrid />
+      </main>
+    </div>
+  );
+}
