11cd84179ea8904a88e4459d9e8fd080103f8671
btvert <0x65d@proton.me>
2025-05-02 13:00:13 -0400

    Update page.tsx

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -8,7 +8,7 @@ export default function UserPage({ params }) {
   return (
     <div className="h-screen grid place-items-center text-center px-6">
       <main className="flex flex-col gap-6 items-center">
-        <h1 className="text-2xl font-bold">{username}'s Board</h1>
+        <h1 className="text-2xl font-bold">{username}'s Lot</h1>
         <UserGrid />
       </main>
     </div>
