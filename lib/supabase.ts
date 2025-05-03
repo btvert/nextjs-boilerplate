f2294714eeb7195db55092a860b4d9263a83f023
btvert <0x65d@proton.me>
2025-05-03 16:49:26 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,7 @@
 // lib/supabase.ts
 import { createBrowserClient } from "@supabase/ssr";
-import { Database } from "@/types/supabase";
 
-const supabase = createBrowserClient<Database>(
+const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
